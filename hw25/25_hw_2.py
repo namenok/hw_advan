df2444fcdbb28f677f9bc9d4663130870eee3150
namenok <arinaznamenok@gmail.com>
2024-06-21 10:58:06 +0300

    25 ( ex.2+

--- hw25/25_hw_2.py
+++ hw25/25_hw_2.py
@@ -1,12 +1,24 @@
 import requests
 import json
 
-URL = 'https://hacker-news.firebaseio.com/v0/item/19155826.json'
-
+URL_2 = 'https://hacker-news.firebaseio.com/v0/topstories.json'
 headers = {'User-Agent': 'Mozilla/5.0'}
-response = requests.get(URL, headers=headers)
-# print(response.status_code)
-article_dict = response.json()
+response = requests.get(URL_2, headers=headers)
+article_list_url_2 = response.json()
 
-print(str(article_dict))
+for ident in article_list_url_2:
+
+    st = 'https://hacker-news.firebaseio.com/v0/item/'
+    id_from_li = str(ident)
+    f = '.json'
+    re = st + id_from_li + f
+    URL = re
+
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(URL, headers=headers)
+    article_dict = response.json()
+
+    print(f'Title : {article_dict["title"]}')
+    print(f'URL : {article_dict["url"]}')
+    print(f'By : {article_dict["by"]}\nID : {article_dict["id"]}\nType:  {article_dict["type"]}')
 
